ba1bd09d27bd37a3aff406033677bf2bc2316686
Adnan Jawed <adnanj383@gmail.com>
2024-01-21 23:02:14 +0530

    Added user info

--- TestAutomationFramework/src/test/java/testcase/MyFIrstTestCaseFW.java
+++ TestAutomationFramework/src/test/java/testcase/MyFIrstTestCaseFW.java
@@ -1,6 +1,9 @@
 package testcase;
 
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
@@ -13,5 +16,10 @@ public class MyFIrstTestCaseFW extends BaseTest {
 			System.out.println("The url of the current webpage is: "+currenturl);
 			String title = driver.getTitle();
 			System.out.println("The title of the webpage is: "+title);
+			driver.findElement(By.xpath("//button[@id = 'login']")).click();
+			driver.findElement(By.xpath("//input[@id = 'userName']")).sendKeys("Adnan");
+			driver.findElement(By.xpath("//input[@id = 'password']")).sendKeys("Games@221998");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.findElement(By.xpath("//form[@id = 'userForm']/div[4]/div/button")).click();
 		}
 }
