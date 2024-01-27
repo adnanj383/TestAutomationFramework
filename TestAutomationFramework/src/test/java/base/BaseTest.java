17aac78c9cfa25dc22994a8cf11e631cf8500b9c
Adnan Jawed <adnanj383@gmail.com>
2024-01-27 18:30:52 +0530

    Added data provider annotation

--- TestAutomationFramework/src/test/java/base/BaseTest.java
+++ TestAutomationFramework/src/test/java/base/BaseTest.java
@@ -6,7 +6,9 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,7 +19,7 @@ public class BaseTest {
 	public static Properties prop = new Properties();
 	public static FileReader fr;
 	
-	@BeforeTest
+	@BeforeMethod
 	public void setUp() throws IOException
 	{
 		if(driver == null)
@@ -46,7 +48,7 @@ public class BaseTest {
 		}
 		
 	}
-	@AfterTest
+	@AfterMethod
 	public void tearDown()
 	{
 		driver.close();
--- TestAutomationFramework/src/test/java/testcase/MyFIrstTestCaseFW.java
+++ TestAutomationFramework/src/test/java/testcase/MyFIrstTestCaseFW.java
@@ -2,24 +2,45 @@ package testcase;
 
 
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.JavascriptExecutor;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 public class MyFIrstTestCaseFW extends BaseTest {
 
-		@Test
-		public static void LoginTest() throws InterruptedException
+		@Test(dataProvider = "testdata")
+		public static void LoginTest(String username, String password) throws InterruptedException
 		{
 			String currenturl = driver.getCurrentUrl();
 			System.out.println("The url of the current webpage is: "+currenturl);
 			String title = driver.getTitle();
 			System.out.println("The title of the webpage is: "+title);
 			driver.findElement(By.xpath("//button[@id = 'login']")).click();
-			driver.findElement(By.xpath("//input[@id = 'userName']")).sendKeys("Adnan");
-			driver.findElement(By.xpath("//input[@id = 'password']")).sendKeys("Games@221998");
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("window.scrollBy(0,250)", "");
+			driver.findElement(By.xpath("//input[@id = 'userName']")).sendKeys(username);
+			driver.findElement(By.xpath("//input[@id = 'password']")).sendKeys(password);
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
+			//Thread.sleep(2000);
 			driver.findElement(By.xpath("//form[@id = 'userForm']/div[4]/div/button")).click();
+			Thread.sleep(2000);
 		}
+@DataProvider(name = "testdata" )
+public Object[][] tData()
+{return new Object[][]
+{
+	{"Adnan","Games221998"},
+	{"Adnan","Games@221998"},
+
+};
 }
+
+
+
+}
+
+
